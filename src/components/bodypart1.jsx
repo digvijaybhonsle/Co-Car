a5da8c0e1086bc1e388291aa398225cbb90bdec3
digvijaybhonsle <digvijaybhonsle007@gmail.com>
2025-03-11 22:01:10 +0530

    changed body1 font

--- src/components/bodypart1.jsx
+++ src/components/bodypart1.jsx
@@ -9,7 +9,6 @@ const Landingbody1 = () => {
   const handleClick = () => {
     navigate("/contact");
   };
-
   return (
     <div className="w-full px-6 py-4 z-[-1]">
       <div className="flex flex-col lg:flex-row w-full lg:h-[700px] bg-white py-6">
@@ -20,38 +19,36 @@ const Landingbody1 = () => {
             src={App2}
             alt="App2 animation"
             loading="lazy"
-            className="absolute top-[10%] left-[10%] w-1/3 sm:w-1/3 lg:w-1/3 h-auto object-cover opacity-80 animate-pulse"
+            className="absolute top-[10%] left-[15%] w-1/2 h-auto object-cover opacity-80 animate-pulse delay-3000 mr-15"
           />
-
           {/* Background Animation - First GIF */}
           <img
             src={App1}
             alt="App1 animation"
             loading="lazy"
-            className="absolute top-[30%] left-[5%] w-1/3 sm:w-1/3 lg:w-1/3 h-auto object-cover opacity-80 animate-pulse"
+            className="absolute top-[30%] left-0 w-1/2 h-auto object-cover opacity-80 animate-pulse"
           />
-
-          {/* Foreground Image (Man) */}
+          {/* Foreground Image */}
           <img
             src={Man}
             alt="Landing visual"
             loading="lazy"
-            className="w-[60%] sm:w-[400px] lg:w-[500px] h-auto object-contain rounded-lg relative lg:ml-24"
+            className="w-[80%] sm:w-[500px] lg:w-[600px] h-auto object-contain rounded-lg"
           />
         </div>
 
         {/* Text Section */}
         <div className="w-full lg:w-1/2 flex flex-col justify-center items-start px-4 sm:px-6 lg:px-12 space-y-4 sm:space-y-6">
           {/* Headings */}
-          <h1 className="text-[#38393C] font-bold text-2xl sm:text-3xl lg:text-4xl leading-tight sm:leading-snug sm:mb-4">
+          <h1 className="text-[#38393C] font-bold text-xl sm:text-2xl lg:text-5xl leading-tight sm:leading-snug sm:mb-4">
             Transforming Co-Commuting:
           </h1>
-          <h1 className="text-[#4CAF50] font-bold text-2xl sm:text-3xl lg:text-4xl leading-tight sm:leading-snug mt-0">
+          <h1 className="text-[#4CAF50] font-bold text-xl sm:text-2xl lg:text-5xl leading-tight sm:leading-snug mt-0">
             Smarter, Greener, Together
           </h1>
 
           {/* Description */}
-          <p className="text-[#323131] text-xl sm:text-lg lg:text-base leading-relaxed max-w-full sm:max-w-[90%] lg:max-w-[75%]">
+          <p className="text-[#323131] text-4xl md:text-3xl sm:text-base lg:text-xl leading-relaxed max-w-full sm:max-w-[90%] lg:max-w-[75%]">
             Revolutionizing urban mobility through innovative, sustainable, and
             technology-driven car-sharing solutions, fostering efficiency,
             safety, and environmental responsibility.
