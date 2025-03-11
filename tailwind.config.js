8021abb4ea04b268a650b7df0d638f7baaa31e45
digvijaybhonsle <digvijaybhonsle007@gmail.com>
2025-03-11 16:42:41 +0530

    new commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,14 @@
-// tailwind.config.js
 export default {
-    content: [
-      './index.html',
-      './src/**/*.{js,ts,jsx,tsx,html}',
-    ],
-    theme: {
-      extend: {
-        fontFamily: {
-          poppins: ['Poppins', 'sans-serif'],
-        },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
       },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
